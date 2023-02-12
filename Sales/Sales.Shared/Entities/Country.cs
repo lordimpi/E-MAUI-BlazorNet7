8e46470404322793aa0557a52db266cca1188ba0
Santiago Acuña <snt-26@hotmail.com>
2023-02-12 12:18:55 -0500

    create country table

---
+++ Sales/Sales.Shared/Entities/Country.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sales.Shared.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Pais")]
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        [Required(ErrorMessage = "El campo {0} el obligatorio")]
+        public string Name { get; set; } = null!;
+
+    }
+}
